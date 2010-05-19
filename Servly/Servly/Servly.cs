cf7f294e5129cc4af15659c3896ebb3a1c2d63de
Josh <josh@bluescripts.net>
2010-05-19 09:42:30 -0400

    first

---
+++ Servly/Servly/Servly.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Management;
+using System.Net;
+using System.IO;
+
+using Echevil; // networking code used from: http://netcode.ru/dotnet/?lang=&katID=30&skatID=277&artID=7659
+
+namespace Servly
+{
+    class Servly
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string servlyUrl = args[0];
+                Console.WriteLine(args[0]);
+            
+                Dictionary<string,string> postData = new Dictionary<string,string>();
+
+                Dictionary<string, double> Disk = DiskUsage();
+                Dictionary<string, double> Memory = MemoryUsage();
+                Dictionary<string, double> Network = NetworkUsage();
+                double cpuUsage = CpuUsage();
+                int Procs = ProcsRunning();
+                double NetConns = NetworkConnections();
+
+                Console.WriteLine("Processors: {0}", Environment.ProcessorCount);
+
+                Console.WriteLine("Operating System: {0}", Environment.OSVersion.ToString());
+
+                Console.WriteLine("Running Processes: {0}", Procs);
+
+                Console.WriteLine("Memory Usage: " + (Memory["availableMemory"] / Memory["totalMemory"]) * 100 + "%");
+
+                Console.WriteLine("CPU Usage: " + cpuUsage + "%");
+
+                Console.WriteLine("Network Connections: " + NetConns);
+
+
+                Console.WriteLine("Disk Free/Used/Total: {0} / {1} / {2}", Disk["free"] / (1024 * 1024 * 1024), Disk["used"] / (1024 * 1024 * 1024), (Disk["free"] + Disk["used"]) / (1024 * 1024 * 1024));
+
+                Console.WriteLine("Upload/Download: {0}/{1}", Network["upload"], Network["download"]);
+
+                postData.Add("os", "Windows");
+                postData.Add("cpu_free", (100 - cpuUsage).ToString());
+                postData.Add("disk_used", Disk["used"].ToString() );
+                postData.Add("disk_size", (Disk["free"] + Disk["used"]).ToString() );
+                postData.Add("mem_used", (Memory["totalMemory"] - Memory["availableMemory"] ).ToString() );
+                postData.Add("mem_free", Memory["availableMemory"].ToString() );
+                postData.Add("procs", Procs.ToString() );
+                postData.Add("net_in", Network["download"].ToString() );
+                postData.Add("net_out", Network["upload"].ToString());
+                postData.Add("ncpus", Environment.ProcessorCount.ToString() );
+                postData.Add("kernel", Environment.OSVersion.ToString() );
+                postData.Add("connections", NetConns.ToString() );
+                postData.Add("ps", ProcList());
+
+
+
+                // this is what we are sending
+                string post_data = Serialize(postData);
+                Console.WriteLine("Post data: " + post_data);
+
+                // this is where we will send it
+                string uri = servlyUrl;
+
+                // create a request
+                HttpWebRequest request = (HttpWebRequest)
+                WebRequest.Create(uri); request.KeepAlive = false;
+                request.ProtocolVersion = HttpVersion.Version10;
+                request.Method = "POST";
+
+                // turn our request string into a byte stream
+                byte[] postBytes = Encoding.ASCII.GetBytes(post_data);
+
+                // this is important - make sure you specify type this way
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = postBytes.Length;
+                Stream requestStream = request.GetRequestStream();
+
+                // now send it
+                requestStream.Write(postBytes, 0, postBytes.Length);
+                requestStream.Close();
+
+                // grab te response and print it out to the console along with the status code
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Console.WriteLine(new StreamReader(response.GetResponseStream()).ReadToEnd());
+                Console.WriteLine(response.StatusCode);
+
+                //Console.Read();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("You need to provide the URL");
+            }
+
+        }
+
+        private static string Serialize(Dictionary<string,string> input)
+        {
+            string str = "";
+            foreach (KeyValuePair<string, string> k in input)
+            {
+                str += "srvly[" + k.Key + "]=" + k.Value + "&";
+            }
+            return str;
+        }
+
+        private static Dictionary<string, double> NetworkUsage()
+        {
+            Dictionary<string, double> temp = new Dictionary<string, double>();
+
+            double DownloadSpeed = 0;
+            double UploadSpeed = 0;
+
+            NetworkMonitor monitor = new NetworkMonitor();
+            NetworkAdapter[] adapters = monitor.Adapters;
+
+            // Start a timer to obtain new performance counter sample every second.
+            monitor.StartMonitoring();
+
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (NetworkAdapter adapter in adapters)
+                {
+                    // The DownloadSpeedKbps and UploadSpeedKbps are
+                    // double values. 
+                   
+                    DownloadSpeed += adapter.DownloadSpeedKbps;
+                    UploadSpeed += adapter.UploadSpeedKbps;
+                }
+                System.Threading.Thread.Sleep(1000); // Sleeps for one second.
+            }
+
+            // Stop the timer. Properties of adapter become invalid.
+            monitor.StopMonitoring();
+
+
+            temp.Add("upload", UploadSpeed);
+            temp.Add("download", DownloadSpeed);
+
+            return temp;
+        }
+
+        private static string ProcList()
+        {
+            string str = "";
+            Process[] processlist = Process.GetProcesses();
+            foreach(Process p in processlist){
+                str += "* \t " + p.Id +" \t * \t * \t * \t * \t * " + p.ProcessName + "\n";
+            }
+
+            return str;
+        }
+
+        private static int ProcsRunning()
+        {
+            Process[] processlist = Process.GetProcesses();
+            return processlist.Length;
+        }
+
+        private static double CpuUsage()
+        {
+            // Get CPU usage over 20 seconds
+            PerformanceCounter cpuCounter;
+            double cpuUsage = 0;
+            cpuCounter = new PerformanceCounter();
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+            for (int i = 0; i < 20; i++) { cpuUsage += cpuCounter.NextValue(); Thread.Sleep(1000); }
+            cpuUsage /= 20;
+
+            return cpuUsage;
+        }
+
+        private static Dictionary<string,double> MemoryUsage()
+        {
+            Dictionary<string, double> temp = new Dictionary<string, double>();
+            double totalMemory = 0;
+            double availableMemory = 0;
+            // Get memory usage and information
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+            ManagementObjectSearcher mos2 = new ManagementObjectSearcher("SELECT * FROM Win32_perfRawData_PerfOS_Memory");
+            // total memory
+            foreach (ManagementObject mo in mos.Get()) { totalMemory = System.Convert.ToDouble(mo["TotalPhysicalMemory"].ToString()); }
+            // available memory
+            foreach (ManagementObject mo in mos2.Get()) { availableMemory = System.Convert.ToDouble(mo["AvailableBytes"].ToString()); }
+
+            temp.Add("totalMemory", totalMemory);
+            temp.Add("availableMemory", availableMemory);
+
+            return temp;
+        }
+
+        private static double NetworkConnections()
+        {
+            // Network Information
+            System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c netstat -an");
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            // Do not create the black window.
+            procStartInfo.CreateNoWindow = true;
+            // Now we create a process, assign its ProcessStartInfo and start it
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            // Get the output into a string
+            double result = proc.StandardOutput.ReadToEnd().ToString().Split('\n').Length;
+            // Display the command output.
+            return result;
+        }
+
+        private static Dictionary<string, double> DiskUsage()
+        {
+
+            Dictionary<string, double> temp = new Dictionary<string, double>();
+
+            double freeSpace = 0;
+            double usedSpace = 0;
+            
+            // get disk stats
+            System.Management.ObjectQuery oQuery = new System.Management.ObjectQuery("select FreeSpace,Size,Name from Win32_LogicalDisk where DriveType=3");
+            ManagementObjectSearcher oSearcher = new ManagementObjectSearcher(oQuery);
+            ManagementObjectCollection oReturnCollection = oSearcher.Get();
+            
+            //loop through found drives and write out info
+            foreach (ManagementObject oReturn in oReturnCollection)
+            {
+                //Free space in MB
+                freeSpace += Convert.ToInt64(oReturn["FreeSpace"]);
+
+                //Used space in MB
+                usedSpace += (Convert.ToInt64(oReturn["Size"]) - Convert.ToInt64(oReturn["FreeSpace"]));
+            }
+            temp.Add("used", usedSpace);
+            temp.Add("free", freeSpace);
+
+            return temp;
+        }
+    }
+}
---
+++ servly-bsd.py
@@ -0,0 +1,208 @@
+#!/usr/local/bin/python
+
+############### INSTALLATION NOTES ###############
+#### Packages/Steps to install
+#--- cd /usr/ports/lang/python; make && make install
+
+#--- cd /usr/ports/sysutils/bsdsar; make && make install
+#--- cd /usr/ports/sysutils/muse; make && make install
+
+### PERL
+
+#### FREEBSD 7
+#--- cd /usr/ports/lang/perl5; make && make install
+#--- ln -s /usr/local/bin/perl /usr/bin/perl
+
+#### FREEBSD 8
+#--- cd /usr/ports/lang/perl5.8; make && make install
+
+
+# tested on: FreeBSD 7.x
+# Need to install Python + Sar
+# /usr/ports/lang/python & /usr/ports/sysutils/bsdsar
+# you also need to make sure perl works and is in /usr/bin/perl
+# for some reason bsd installs it in /usr/local/bin but bsdsar looks in 
+# /usr/bin
+# Also for memory reporting to work properly you need to install muse
+# /usr/ports/sysutils/muse
+# that should be it :) 
+# lots of abs paths were required as cron didnt pick up PATHs properly
+# may need to adjust depending on your bsd env
+import urllib2, urllib, socket, os, statvfs, commands
+servlyapp = "http://DOMAIN.servly.com/status/update/KEY"
+
+load = os.getloadavg()
+disk = os.statvfs(".")
+
+# kernel info
+kernel = os.popen("uname -a").readline()
+
+# operating system release info
+release_info = os.popen("cat /etc/*release").readline()
+
+#procs
+procs = os.popen("ps ax | grep -c ''").readline()
+
+#os
+opsys = os.popen("uname").readline()
+
+#connections 
+conns = int(commands.getoutput("netstat -an | grep -c ':'"))
+
+
+# bytes
+disk_free = (disk.f_bavail * disk.f_frsize)
+disk_size = (disk.f_blocks * disk.f_frsize)
+disk_used = disk_size - disk_free
+
+# memory usage - EXPECTS BYTES TO BE SENT SO MAKE SURE YOU MULTIPLY BY PROP VALUE
+free_memory = 0
+used_memory = 0
+free = os.popen("/usr/local/bin/muse | tail -n 1 | awk '{print $2}'").readline()
+total = os.popen("/usr/local/bin/muse | tail -n 2 | head -n 1 | awk '{print $2}' ").readline()
+
+free_memory = float(free) # already in bytes
+used_memory = float(total)-free_memory # already in bytes
+
+# get num of cpus
+ncpus = int(os.popen("sysctl hw.ncpu | awk '{ print $2 }'").readline())
+
+# cpu usage
+# windows: http://kahrn.pastebin.com/f2430d665
+# linux: ps -e -o pcpu
+usage = 0
+cpu_free = os.popen("/usr/local/bin/bsdsar -u 30 | awk '{ print $NF }' | tail -n 1").readline()
+
+# lets get network usage now via sar, we need to figure out which columns are what and sum them
+net = commands.getoutput("/usr/local/bin/bsdsar -I").split('\n')
+# look through first line
+find_iface = net[2]
+
+#print find_iface
+in_bytes = 0
+out_bytes = 0
+i = 0
+in_bytes_col = 0
+out_bytes_col = 0
+for h in find_iface.split(' '): #find the position column of the if headers
+    if len(h) > 0:
+        if h == "Ibytes/s" or h == "rxbyt/s":
+           in_bytes_col = i
+        if h == "Obytes/s" or h == "txbyt/s":
+            out_bytes_col = i
+        i+=1
+for c in net[3:-1]:
+    this_c = c.split(' ')
+    x = 0
+    for col in this_c:
+        if len(col) > 0 and this_c[0] != "Average:":
+			try:
+				if x == in_bytes_col:
+					in_bytes+=float(col)
+				if x == out_bytes_col:
+					out_bytes+=float(col)
+			except ValueError:
+				pass
+			x+=1
+
+
+# check for a webserver: nginx, lighttpd, apache (httpd)
+web = 0
+web_lsws = int(os.popen('ps ax | grep lshttpd | grep -v -c grep').readline()) # litespeed
+web_lhttpd = int(os.popen('ps ax | grep lighttpd | grep -v -c grep').readline()) # lighttpd
+web_nginx = int(os.popen('ps ax | grep nginx | grep -v -c grep').readline()) #nginx 
+web_httpd = int(os.popen('ps ax | grep "httpd" | grep -v "interworx" | grep -v "lighttpd" | grep -v -c grep').readline()) #httpd = filter out interworx
+
+
+if web_lsws > 0 or web_lhttpd > 0 or web_nginx > 0 or web_httpd > 0:
+	web = 1
+
+db = 0
+db_mysql = int(os.popen('ps ax | grep mysql | grep -v -c grep').readline()) # mysql
+
+if db_mysql > 0:
+	db = 1
+
+#ftp check
+ftp = 0
+ftp_proftpd = int(os.popen('ps ax | grep proftpd | grep -v -c grep').readline())
+
+if ftp_proftpd > 0:
+	ftp = 1
+
+#ssh check
+ssh = 0
+sshd = int(os.popen('ps ax | grep sshd | grep -v -c grep').readline())
+
+if sshd > 0:
+	ssh = 1
+
+#ssh check
+nfs = 0
+nfsd = int(os.popen('ps ax | grep nfsd | grep -v -c grep').readline())
+
+if nfsd > 0:
+	nfs = 1
+
+#dns check
+dns = 0
+dns_tinydns = int(os.popen('ps ax | grep tinydns | grep -v -c grep').readline())
+
+if dns_tinydns > 0:
+	dns = 1
+
+#mail check
+mail = 0
+mail_qmail = int(os.popen('ps ax | grep qmail | grep -v -c grep').readline())
+
+if mail_qmail > 0:
+	mail = 1
+
+# If you want to track a custom service, uncomment the lines below and do your own checking for whatever service you want
+#custom check
+custom = 0
+#custom_test = int(os.popen('ps ax | grep qmail | grep -v -c grep').readline())
+#if mail_qmail > 0
+#	mail = 1
+
+
+ps_output = os.popen('ps -eo user,pid,ppid,rss,vsize,pcpu,pmem,command --sort vsize')
+ps = ""
+for x in ps_output:
+	ps += x
+
+login = urllib.urlencode(
+    {
+        'srvly[cpu_free]': cpu_free,
+        'srvly[disk_used]': disk_used,
+        'srvly[disk_size]': disk_size,
+        'srvly[mem_used]': used_memory,
+        'srvly[mem_free]': free_memory,
+        'srvly[procs]': procs,
+        'srvly[load_one]': "%.2f" % load[0],
+        'srvly[load_five]': "%.2f" % load[1],
+        'srvly[load_fifteen]': "%.2f" % load[2],
+        'srvly[net_in]': in_bytes,
+        'srvly[net_out]': out_bytes,
+        'srvly[ncpus]': ncpus,
+        'srvly[os]': opsys,
+        'srvly[web]': web,
+        'srvly[db]': db,
+        'srvly[connections]': conns,
+		'srvly[kernel]': kernel,
+		'srvly[release_info]': release_info,
+		'srvly[ftp]': ftp,
+		'srvly[ssh]': ssh,
+		'srvly[nfs]': nfs,
+		'srvly[dns]': dns,
+		'srvly[mail]': mail,
+		'srvly[custom]': custom,
+		'srvly[ps]': ps
+    }
+);
+e = urllib2.urlopen(servlyapp, login)
+print "Status posted to " + servlyapp
+print e.read()
+
+
+
